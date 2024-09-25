3d14e639a97d44ea2d0c8c2926cd7ded9a9ef2b7
devavology <145644105+devavology@users.noreply.github.com>
2024-09-25 19:29:29 +0530

    done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,6 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     document.querySelector(".toggle").addEventListener("click", function () {
-//       document.body.classList.add("menuToggle");
-//     });
-  
-//     document.querySelector(".close").addEventListener("click", function () {
-//       document.body.classList.remove("menuToggle");
-//     });
-//   });
 
-  // AOS.init({
-  //   duration: 1200,
-  // })
+
+
   
 
   // Toggle the mobile menu
